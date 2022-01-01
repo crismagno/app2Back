8b02a5ed81d7ea21b0f0c9d7a33f6a5d892c6d0c
@cmendonca1 <cmendonca@drcontactlens.com>
2022-01-01 15:25:51 -0300

    Add NOD_ENV=dev

--- src/utils/SocketRedis/index.ts
+++ src/utils/SocketRedis/index.ts
@@ -3,13 +3,15 @@ const redis = require("socket.io-redis");
 
 export class SocketRedis {
   public static start(io: any) {
-    try {
-      io.adapter(
-        redis({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT })
-      );
-      Log.show("Starting socket redis...", "SOCKET_SOCIAL", "success");
-    } catch (error) {
-      Log.show("Error socket redis...", "SOCKET_SOCIAL", "error");
+    if (process.env.NODE_ENV === "dev") {
+      try {
+        io.adapter(
+          redis({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT })
+        );
+        Log.show("Starting socket redis...", "SOCKET_SOCIAL", "success");
+      } catch (error) {
+        Log.show("Error socket redis...", "SOCKET_SOCIAL", "error");
+      }
     }
   }
 }
